ec55be78161e7b003d367b1c8206d65699f44b70
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-01 23:58:36 +0530

    Created the logic for the SEND button. Takes the data from the input box, encrypts it and returns an ID of the format "key : string" where key is used for decryption and string used for data storing and retrieval.

--- main.py
+++ main.py
@@ -18,19 +18,29 @@ class MainWindow(QMainWindow):
         self.ui.sendButton.clicked.connect(self.on_send_clicked)
         self.ui.fetchButton.clicked.connect(self.on_fetch_clicked)
 
+    @staticmethod
     def encrypt_string(plain_text: str) -> tuple[bytes, bytes]:
         key = Fernet.generate_key()
         cipher = Fernet(key)
         plain_text_bytes = plain_text.encode('utf-8')
         encrypted_blob = cipher.encrypt(plain_text_bytes)
         return encrypted_blob, key
-
+        
+    @staticmethod
     def random_string(length: int =  7) -> str:
         characters = string.ascii_letters + string.digits + string.punctuation
         return ''.join(random.choice(characters) for _ in range(length))
     
     def on_send_clicked(self):
-        print("Send button clicked")
+        plain_text = self.ui.sendTextEdit.toPlainText()
+        if not plain_text:
+            self.ui.idLabel.setText("ID - (Please enter text to send)")
+            return
+        _encrypted_blob, key = self.encrypt_string(plain_text)
+        rand_str = self.random_string()
+        key_str = key.decode('utf-8')
+        output_display = f"{key_str} : {rand_str}"
+        self.ui.idLabel.setText(output_display)
 
     def on_fetch_clicked(self):
         print("Fetch button clicked")
