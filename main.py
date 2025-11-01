b74133084c9a133f1516d4812070ab922d1b51c4
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-01 23:50:34 +0530

    Directly defined the encrypt_string and random string functions within main.py itself.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import random
+import string
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 from ui_generated import Ui_CommandLineTool
-from encrypt import encrypt_string, random_string
+from cryptography.fernet import Fernet
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -15,6 +17,17 @@ class MainWindow(QMainWindow):
     def connect_signals(self):
         self.ui.sendButton.clicked.connect(self.on_send_clicked)
         self.ui.fetchButton.clicked.connect(self.on_fetch_clicked)
+
+    def encrypt_string(plain_text: str) -> tuple[bytes, bytes]:
+        key = Fernet.generate_key()
+        cipher = Fernet(key)
+        plain_text_bytes = plain_text.encode('utf-8')
+        encrypted_blob = cipher.encrypt(plain_text_bytes)
+        return encrypted_blob, key
+
+    def random_string(length: int =  7) -> str:
+        characters = string.ascii_letters + string.digits + string.punctuation
+        return ''.join(random.choice(characters) for _ in range(length))
     
     def on_send_clicked(self):
         print("Send button clicked")
