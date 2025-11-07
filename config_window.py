50707f30b72fbb5d0c09306ecdc80b11987203b7
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-07 20:02:36 +0530

    Built and verified the functionality of the ConfigWindow. When the OK button is pressed, the database URL and the location of the json file will be written to a text file under the CWD.

--- config_window.py
+++ config_window.py
@@ -8,9 +8,11 @@ class ConfigWindow(QDialog, Ui_ApplicationSetup):
         super().__init__(parent)
         self.setupUi(self)
         self.connect_signals()
+        self.selected_file_path = ""
 
     def connect_signals(self):
         self.selectFileButton.clicked.connect(self.select_file)
+        self.buttonBox.accepted.connect(self.accept)
 
 
     def select_file(self):
@@ -20,10 +22,25 @@ class ConfigWindow(QDialog, Ui_ApplicationSetup):
             "",
             "JSON Files (*.json)"
         )
-
         if file_path:
             self.selected_file_path = file_path
             self.selectedFileLabel.setText(file_path)
+
+    def accept(self):
+        database_url = self.lineEdit_url.text().strip()
+        if not database_url:
+            print("Please enter a valid database URL.")
+            return
+
+        if not self.selected_file_path:
+            print("Please select a valid Firebase Admin Credentials file.")
+            return
+        
+        with open("config.txt", "w") as config_file:
+            config_file.write(f"{database_url}\n")
+            config_file.write(f"{self.selected_file_path}\n")
+        super().accept()      
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = ConfigWindow()
