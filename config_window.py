b58a91d598d1697765182627dcb6e887db07d851
SIDDHARTH K P <siddhubhai998@gmail.com>
2025-11-07 19:09:52 +0530

    Added ConfigWindow class to implement application setup UI. Just some boilerplate code.

--- config_window.py
+++ config_window.py
@@ -1,1 +1,19 @@
+import sys
+from PyQt6 import QtWidgets, QtCore
+from PyQt6.QtWidgets import QDialog, QFileDialog, QApplication
 from configs import Ui_ApplicationSetup
+
+class ConfigWindow(QDialog, Ui_ApplicationSetup):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connect_signals()
+
+    def connect_signals(self):
+        pass
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ConfigWindow()
+    window.show()
+    sys.exit(app.exec())
